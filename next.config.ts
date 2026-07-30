7a17a734db476eebce35ffa096c6a5ad00e01b5e
federicobisi-cloud <federicobisi@gmail.com>
2026-07-30 10:12:19 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         destination: 'https://vinito-wheelwright-production.up.railway.app/:path*',
         permanent: false,
       },
+      {
+        source: '/administracion/:path*',
+        destination: 'https://vinito-gestion-production.up.railway.app/:path*',
+        permanent: false,
+      },
     ];
   },
 };
